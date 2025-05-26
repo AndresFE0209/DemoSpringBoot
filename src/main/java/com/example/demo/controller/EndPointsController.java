1b4d5bb730f12e6cb9c8dd8f485bf3a8a518acbd
AndresFE0209 <afernandeze10@iesalbarregas.es>
2025-05-26 11:23:23 +0200

    Generacion endpoints

--- src/main/java/com/example/demo/controller/EndPointsController.java
+++ src/main/java/com/example/demo/controller/EndPointsController.java
@@ -3,15 +3,23 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class EndPointsController {
     @GetMapping("/endpoints1")
-    public String endpoints1() {
-        return "Endpoints 1";
+    public Map<String, String>endpoints1() {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Endpoints 1");
+        return response;
     }
 
+
     @GetMapping("/endpoints2")
-    public String endpoints2() {
-        return "Endpoints 2";
+    public Map<String, String>endpoints2() {
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Endpoints 2");
+        return response;
     }
 }
